5d4a0cd45bfb9deac34510ee1c6dac6b57386b9b
KonjkovVV <Konjkov.VV@gmail.com>
2017-03-04 12:56:01 +0700

    * other basis MP2

--- NWCHEM/C8H8/gnuplot.sh
+++ NWCHEM/C8H8/gnuplot.sh
@@ -2,7 +2,7 @@
 
 set grid
 
-plot "bonds.dat" using 2:3 with linespoints notitle,\
-     "bonds.dat" using 2:3:1 with labels offset -6.0,-0.5 font "arial,8" notitle,\
+plot "bonds.dat" using ($2*2):(($3-$2)*sqrt(3)) with linespoints notitle,\
+     "bonds.dat" using ($2*2):(($3-$2)*sqrt(3)):1 with labels offset -6.0,-0.5 font "arial,8" notitle,\
      "equlibrium.dat" using 2:3:4:5 with xyerrorbars notitle,\
      "equlibrium.dat" using 2:3:1 with labels offset -6.0,-0.5 font "arial,8" notitle
