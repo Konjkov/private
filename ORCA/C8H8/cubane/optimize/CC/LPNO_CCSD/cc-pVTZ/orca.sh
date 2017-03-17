23ca788c8f51866b7d6b2e553bca5a050d72ec93
Vladimir Konkov <VladimirK@meforo.com>
2017-03-17 11:50:27 +0700

    add more

---
+++ ORCA/C8H8/cubane/optimize/CC/LPNO_CCSD/cc-pVTZ/orca.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+task_dir=`pwd`
+
+ORCA_PATH=/home/vladimir/bin/orca_3_0_3_linux_x86-64
+export PATH=$PATH:$ORCA_PATH
+
+for dir in '.'
+do
+    if [ ! -e $task_dir/$dir/cubane.out ];
+    then
+        $ORCA_PATH/orca $task_dir/$dir/cubane.inp > $task_dir/$dir/cubane.out &
+    else
+        echo "file $task_dir/$dir/cubane.out exists, skiping execution"
+    fi
+done
