903d7bf508fb784a03d91b0adc9c3a6930cdc0f6
Vladimir Konkov <VladimirK@meforo.com>
2017-03-17 11:19:20 +0700

    * add ORCA

---
+++ ORCA/gnuplot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/gnuplot -persist
+
+set grid
+
+plot "bonds.dat" using ($2*2):(($3-$2)*sqrt(3)) with linespoints notitle,\
+     "bonds.dat" using ($2*2):(($3-$2)*sqrt(3)):1 with labels offset -6.0,-0.5 font "arial,8" notitle,\
+     "equlibrium.dat" using 2:3:4:5 with xyerrorbars notitle,\
+     "equlibrium.dat" using 2:3:1 with labels offset -6.0,-0.5 font "arial,8" notitle
+
+#plot "energy.dat" using 2:3 with linespoints notitle,\
+#     "energy.dat" using 2:3:1 with labels offset -6.0,-0.5 font "arial,8" notitle
